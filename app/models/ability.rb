b43d0b42cd2e36c92dc6c0ab4561ed3f8b556e15
alex07L <alexlevesque7@hotmail.fr>
2020-07-17 23:38:37 -0400

    add employee grade

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,9 +4,35 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-	return unless user && (user.admin == true || !Customer.find_by(user_id:user.id).nil?)
+	return unless user && (user.admin == true || !Customer.find_by(user_id:user.id).nil?|| !Employee.find_by(user_id:user.id).nil?)
 	if user.admin == true
 		can :manage, :all
+	elsif !Employee.find_by(user_id:user.id).nil?
+		can :access, :rails_admin
+		can :read, :dashboard  
+		can :index, Dwhquote
+		can :charts, Dwhquote
+		can :index, Dwhlead
+		can :charts, Dwhlead
+		can :index, Dwhcustomer
+		can :charts, Dwhcustomer
+		can :manage, User,["id = ?", user.id] do |user|
+        user.present?
+		end
+		can :manage, Employee,["id = ?", Employee.find_by(user_id:user.id).id] do |user|
+        user.present?
+		end
+		can :manage, Battery,["employee_id = ?", Employee.find_by(user_id:user.id).id] do |user|
+			user.present?
+		end
+		can :manage, Column,["battery_id = ?", Battery.find_by(employee_id: Employee.find_by(user_id:user.id).id).id] do |user|
+			user.present?
+		end
+		can :manage, Elevator,["column_id = ?", Column.find_by(battery_id: Battery.find_by(employee_id: Employee.find_by(user_id:user.id).id).id).id] do |user|
+			user.present?
+		end
+		can :read, Type
+		can :read, Category
 	else
 		can :access, :rails_admin
 		can :read, :dashboard  
@@ -16,6 +42,26 @@ class Ability
 		can :charts, Dwhlead
 		can :index, Dwhcustomer
 		can :charts, Dwhcustomer
+		can :manage, User,["id = ?", user.id] do |user|
+        user.present?
+		end
+		can :read, Customer,["id = ?", Customer.find_by(user_id:user.id).id] do |user|
+        user.present?
+		end
+		can :manage, Address,["id = ?", Customer.find_by(user_id:user.id).address] do |user|
+        user.present?
+		end
+		can :read, Building,["customer_id = ?", Customer.find_by(user_id:user.id).id] do |user|
+        user.present?
+		end
+		can :read, BuildingDetail,["building_id = ?", Customer.find_by(user_id:user.id).building.ids] do |user|
+        user.present?
+		end
+		can :read, Battery,["building_id = ?", Customer.find_by(user_id:user.id).building.ids] do |user|
+        user.present?
+		end
+		can :read, Type
+		can :read, Category
 	end
     # Define abilities for the passed in user here. For example:
     #
