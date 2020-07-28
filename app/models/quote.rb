d2a60796f98f39f082c5a06ce42421737a14eb9c
THIERRY HARVEY <THIERRYH9@GMAIL.COM>
2020-07-28 12:59:59 -0400

    working quote

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -10,21 +10,21 @@ class Quote < ApplicationRecord
           config.username = "thierryh9@gmail.com"
           config.token = "0uSkLGLqUN9vAwypvb3wXGl9QJBQFAMTPkMz0hus"
       end
-      ZendeskAPI::Ticket.create!(client, 
-          :subject => "#{self.fullName} from #{self.companyName}", 
-          :comment => { 
-              :value => "The contact #{self.Full_Name} from company #{self.Company_Name} 
-                  can be reached at email #{self.Email} and at phone number #{self.Phone_Number}. 
-                  Building type selected is #{self.Building_Type} with product line #{self.Product_Grade}. 
-                  Number of suggested elevator is #{self.Nb_Ele_Suggested} and total price is #{self.Final_Price}. \n
-                  For More Information, refers to Quote ##{self.id}."
-          }, 
-          :requester => { 
-              "name": self.Full_Name, 
-              "email": self.Email 
-          },
-          :priority => "normal",
-          :type => "task"
-          )
+#       ZendeskAPI::Ticket.create!(client, 
+#           :subject => "#{self.fullName} from #{self.companyName}", 
+#           :comment => { 
+#               :value => "The contact #{self.Full_Name} from company #{self.Company_Name} 
+#                   can be reached at email #{self.Email} and at phone number #{self.Phone_Number}. 
+#                   Building type selected is #{self.Building_Type} with product line #{self.Product_Grade}. 
+#                   Number of suggested elevator is #{self.Nb_Ele_Suggested} and total price is #{self.Final_Price}. \n
+#                   For More Information, refers to Quote ##{self.id}."
+#           }, 
+#           :requester => { 
+#               "name": self.Full_Name, 
+#               "email": self.Email 
+#           },
+#           :priority => "normal",
+#           :type => "task"
+#           )
   end
 end
