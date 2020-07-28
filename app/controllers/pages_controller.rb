0f31e3837b816ba6f98e073956bfe41e2c250701
alex07L <alexlevesque7@hotmail.fr>
2020-07-28 10:32:47 -0400

    fix

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -9,9 +9,9 @@ class PagesController < ApplicationController
   end
   def create
     
-    cat = Category.where(:name => params[:gamme]).first
-    building = Type.where(:name => params[:building]).first
-    quote = Quote.create(companyName: params[:companyName],email: params[:email],floor: to_number(params[:floor]),basement: to_number(params[:basement]),apartment: to_number(params[:apartment]),business: to_number(params[:commerce]),shaft: to_number(params[:shafts]),companie: to_number(params[:companie]),parking: to_number(params[:parking]),ocupant: to_number(params[:occupant]),open: to_number(params[:time]), category: cat, type: building)
+    cat = ::Category.where(:name => params[:gamme]).first
+    building = ::Type.where(:name => params[:building]).first
+    quote = ::Quote.create(companyName: params[:companyName],email: params[:email],floor: to_number(params[:floor]),basement: to_number(params[:basement]),apartment: to_number(params[:apartment]),business: to_number(params[:commerce]),shaft: to_number(params[:shafts]),companie: to_number(params[:companie]),parking: to_number(params[:parking]),ocupant: to_number(params[:occupant]),open: to_number(params[:time]), category: cat, type: building)
     quote.save()
     redirect_to controller: 'pages'
     
