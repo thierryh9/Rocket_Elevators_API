1f2e93c54409d14f2f23de4b3befd6a83159cc5c
alex07L <alexlevesque7@hotmail.fr>
2020-07-29 00:20:48 -0400

    dropboxx working

--- db/migrate/20200715143651_add_column_to_lead.rb
+++ db/migrate/20200715143651_add_column_to_lead.rb
@@ -1,5 +1,6 @@
 class AddColumnToLead < ActiveRecord::Migration[5.2]
   def change
   add_column :leads, :fileName, :string, default:""
+  add_column :leads, :shareLink, :string, default:""
   end
 end
