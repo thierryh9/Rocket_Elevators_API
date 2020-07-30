869aaf9a41c7e3367f7eae8097422cfdf11770f1
THIERRY HARVEY <THIERRYH9@GMAIL.COM>
2020-07-30 16:09:19 -0400

    ruby version

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,8 +3,8 @@ lock "~> 3.14.1"
 
 before 'deploy', 'rvm1:install:ruby'
 set :rvm_map_bins, [ 'rake', 'gem', 'bundle', 'ruby', 'puma', 'pumactl' ]
-set :application, "AlexandreLevesque"
-set :repo_url, "https://github.com/alex07L/Rocket_Elevators_Information_System.git"
+set :application, "ThierryHarvey"
+set :repo_url, "https://github.com/thierryh9/Rocket_Elevators_API"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
