2d0ea68aed0f365429c087a8530c6bfba0d7a663
alex07L <alexlevesque7@hotmail.fr>
2020-07-30 23:44:37 -0400

    change email message

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -55,8 +55,8 @@ class PagesController < ApplicationController
 	Lead.create(fullName: params[:contact][:name], entrepriseName: params[:contact][:entreprise], email: params[:contact][:email], cellPhone: params[:contact][:phone], projectName: params[:contact][:projectname], description: params[:contact][:describe], type: Type.where(:name => params[:contact][:department]).first, message: params[:contact][:message], file: params[:contact][:attachment].read, fileName: params[:contact][:attachment].original_filename)
 	from = Email.new(email: ENV['EMAIL_SENDGRID'])
 	to = Email.new(email: params[:contact][:email])
-	subject = 'Greetings '+params[:contact][:name]
-	content = Content.new(type: 'text/html', value: '<html><body><a href="http://www.relevator.ca" title="Rocket Elevators"><img src="http://www.relevator.ca'+ActionController::Base.helpers.image_url('R2-01.jpg')+'" alt="Rocket logo" /></a><br />We thank you for contacting Rocket Elevators to discuss the opportunity to contribute to your project'+params[:contact][:projectname]+'. <br> A representative from our team will be in touch with you very soon. We look forward to demonstrate the value of our solutions and help you choose the appropriate product given your requirements.
+	subject = "Rocket Elevator"
+	content = Content.new(type: 'text/html', value: '<html><body><a href="http://www.relevator.ca" title="Rocket Elevators"><img src="http://www.relevator.ca'+ActionController::Base.helpers.image_url('R2-01.jpg')+'" alt="Rocket logo" /></a><br />Greetings '+params[:contact][:name]'<br/>We thank you for contacting Rocket Elevators to discuss the opportunity to contribute to your project'+params[:contact][:projectname]+'. <br> A representative from our team will be in touch with you very soon. We look forward to demonstrate the value of our solutions and help you choose the appropriate product given your requirements.
 We’ll Talk soon
 The Rocket Team</body>
 </html>')
